2178bb318ec31b5a394015a692d051d88168c3a0
ahmed <rifkiahmed.91@gmail.com>
2023-08-12 16:58:57 +0000

    test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,4 +8,14 @@ from models.base_model import BaseModel
 
 class BaseModelTests(unittest.TestCase):
     """ Defines BaseModel tests cases class """
-    pass
+    def test_instance(self):
+        """ test initialization """
+        b = BaseModel()
+        self.assertIsInstance(b.id, str)
+
+    def test_instance(self):
+        """ test initialization """
+        b1= BaseModel()
+        b_dict = b.to_dict()
+        b2 = BaseModel(b_dict)
+        self.assertEqual(b1, b2)
