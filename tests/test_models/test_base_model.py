13ef7c6f35d7f214210645646057824dedd255a7
ahmed <rifkiahmed.91@gmail.com>
2023-08-12 20:24:22 +0000

    add test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,22 +12,12 @@ class BaseModelTests(unittest.TestCase):
     def test_instance(self):
         """ test initialization """
         b = BaseModel()
-        self.assertIsInstance(b.id, str)
-
-    def test_to_dict(self):
-        """ test initialization """
-        b1 = BaseModel()
-        b1_dict = b1.to_dict()
-        self.assertIsInstance(b1_dict, dict)
-
-    def test_str(self):
-        """ test cases """
-        b = BaseModel
-        self.assertIsInstance(str(b), str)
-
-    def test_save(self):
-        """ test save method """
-        b1 = BaseModel()
-        date = b1.updated_at
-        b1.save()
-        self.assertNotEqual(date, b1.updated_at)
+        id = b.id
+        self.assertEqual(b.id, id)
+        date_time = b.created_at
+        b.save()
+        self.assertEqual(date_time, b.created_at)
+        dic = b.to_dict()
+        self.assertEqual(dic, b.to_dict())
+        s = str(b)
+        self.assertEqual(s, str(b))
