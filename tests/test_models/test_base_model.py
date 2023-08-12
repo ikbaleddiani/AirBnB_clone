2b6b840e2b5b65ed1936be4657db8e064d962fb6
ahmed <rifkiahmed.91@gmail.com>
2023-08-12 17:05:33 +0000

    test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,9 +13,16 @@ class BaseModelTests(unittest.TestCase):
         b = BaseModel()
         self.assertIsInstance(b.id, str)
 
-    def test_instance(self):
+    def test_to_dict(self):
         """ test initialization """
         b1= BaseModel()
         b_dict = b.to_dict()
         b2 = BaseModel(b_dict)
         self.assertEqual(b1, b2)
+
+    def test_save(self):
+        """ test save method """
+        b1 = BaseModel()
+        b2 = b1
+        b2.save()
+        self.assertNotEqual(b2.updated_at ,b1.updated_at)
