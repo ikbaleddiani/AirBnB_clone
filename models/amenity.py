c04e96be16e0ac252dde6508a94faa57684e323e
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-08-13 17:38:44 -0700

    new edit

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-    Amenity model inherits from BaseModel class
-"""
+"""Amenity model inherits from BaseModel class"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     """ Defines Amenity class """
+
     name = ""
